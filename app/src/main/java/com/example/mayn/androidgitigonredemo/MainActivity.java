a771564c5ef6e535968a69671f55df037eefbad3
nan <haoyuxi9605@163.com>
2018-12-21 15:54:31 +0800

    create

---
+++ app/src/main/java/com/example/mayn/androidgitigonredemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mayn.androidgitigonredemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
